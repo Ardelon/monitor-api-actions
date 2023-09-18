b58429857002e173f53292a89662a02e40db15d1
Alihan Keskin <akeskinw@gmail.com>
2023-09-18 09:43:47 +0300

    update test

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const { checkServiceStatus } = require("./checkServices");
 
 const serviceListString = process.env.SERVICE_LIST;
 const serviceList = serviceListString.split(",");
+console.log(process.env.MAIL_ADDRES, process.env.MAIL_ADDRESS);
 
 serviceList.forEach((serviceUrl) => {
   checkServiceStatus(serviceUrl);
