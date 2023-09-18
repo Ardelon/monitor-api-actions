fca6d9c7e7b803e50487b2da7bd699a3d07a7d16
Alihan Keskin <akeskinw@gmail.com>
2023-09-18 09:20:33 +0300

    test update

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const { checkServiceStatus } = require("./checkServices");
 require("dotenv").config();
 
-console.log(process.env.SERVICE_LIST);
 const serviceList = JSON.parse(process.env.SERVICE_LIST);
 serviceList.forEach((serviceUrl) => {
   checkServiceStatus(serviceUrl);
