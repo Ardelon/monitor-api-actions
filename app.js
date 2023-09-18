a7d9853eabf989495c6a2fba8320b34a4d524f5b
Alihan Keskin <akeskinw@gmail.com>
2023-09-18 09:51:08 +0300

    update test

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const serviceListString = process.env.SERVICE_LIST;
 const serviceList = serviceListString.split(",");
 console.log(
   process.env.SERVICE_LIST,
-  process.env.MAIL_ADDRES,
+  process.env.MAIL_KEY,
   process.env.MAIL_ADDRESS
 );
 console.log("selam");
